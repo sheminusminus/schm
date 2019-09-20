65fcf80641433999664a5ac932e0bda89b0d2ccc
Emily Kolar <emkolar@gmail.com>
2019-09-19 20:04:31 -0500

    export isSchema util

--- packages/schm/src/index.js
+++ packages/schm/src/index.js
@@ -2,8 +2,10 @@ import schema, { group } from "./schema";
 import parse from "./parse";
 import validate from "./validate";
 import mapValues from "./mapValues";
+import { isSchema } from "./utils";
 
 module.exports = schema;
+module.exports.isSchema = isSchema;
 module.exports.group = group;
 module.exports.parse = parse;
 module.exports.validate = validate;
